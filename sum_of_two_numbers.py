e612270aebd4dc8c2986bcfe3fd49c981917d893
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2026-06-26 22:07:04 +0600

    File name: sum_of_two_numbers.py
    This code accepts numbers from the user and calculates their sum.

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,10 @@
+""" Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: sum_of_two_numbers.py """
+
+number_1 = float(input("Enter the first number: "))      # The first number of the user
+number_2 = float(input("Enter the second number: "))     # The second number of the user
+
+sum = number_1 + number_2     #Calculation
+
+print("The sum is", sum)
