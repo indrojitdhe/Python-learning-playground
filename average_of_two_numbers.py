2f24eff2f75f10e0324093ed815cd1d989742735
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2026-07-01 20:19:12 +0600

    File name: average_of_two_numbers.py
    This code calculates and displays the average of two numbers.

---
+++ average_of_two_numbers.py
@@ -0,0 +1,12 @@
+""" Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: average_of_two_numbers.py """
+
+number_1 = float(input("Enter the first number: "))      # The first number of the user
+number_2 = float(input("Enter the second number: "))     # The second number of the user
+
+#Calculation
+sum = number_1 + number_2
+avg = sum/2    
+
+print(f"The result is {avg}")
