f665d28d1802684b5d1d8dd790ad82a5158f2552
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2026-07-03 23:55:24 +0600

    File name: check_even_odd.py
    This code checks whether a number is even or odd.

---
+++ check_even_odd.py
@@ -0,0 +1,11 @@
+""" Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: check_even_odd.py """
+
+num = float(input("Enter the number: "))
+
+if(num % 2 == 0):
+    print("Even number.")
+
+else:
+    print("Odd number.")
