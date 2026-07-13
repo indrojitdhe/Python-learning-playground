4a5e80ea9a86e6c8a0695a3075c6feb3314fea65
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2026-07-13 23:56:53 +0600

    File name: mod_value.py
    This code calculates and displays the remainder using the modulus operator.

---
+++ mod_value.py
@@ -0,0 +1,10 @@
+""" Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: mod_value.py """
+
+number_1 = float(input("Enter the first number: "))      # The first number of the user
+number_2 = float(input("Enter the second number: "))     # The second number of the user
+
+mod = (number_1 % number_2)     #Calculation
+
+print("The result is", mod)
