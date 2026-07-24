cf50ae8dc481f191a8abcc1b4a9faa92e8e1fc25
INDROJIT DHE SHAON <indrojitdheshaon@gmail.com>
2026-07-24 23:51:16 +0600

    File name: multiply_two_numbers.py
    This code multiplies two numbers.

---
+++ multiply_two_numbers.py
@@ -0,0 +1,10 @@
+""" Author: INDROJIT DHE SHAON
+Personal Code: 17
+File: multiply_two_numbers.py """
+
+number_1 = float(input("Enter the first number: "))      # The first number of the user
+number_2 = float(input("Enter the second number: "))     # The second number of the user
+
+mul = number_1 * number_2     #Calculation
+
+print(f"The result is {mul}")
